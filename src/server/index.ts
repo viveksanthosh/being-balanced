cff4777ae1730bbed638a989ed4f44fdca46c837
vivek <viveksanthosh.in@gmail.com>
2024-04-01 20:12:08 +0530

    feat: added cookie/body parser

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 
 import routes from "./routes";
-
 import "./database";
 
 export const app = express();
+app.use(helmet());
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api", routes);
